c27d67382e2d1b34793d6bbe60d162387bd9465e
Saswata Kumar Nayak <saswatworkspace711@gmail.com>
2025-06-18 22:06:18 +0530

    Add files via upload
    week - 1 solution day 2

---
+++ Design Pattern & Principles/SingletonPatternExample/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First log message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second log message");
+
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same (Singleton works)");
+        } else {
+            System.out.println("Different instances exist (Singleton failed)");
+        }
+    }
+}
