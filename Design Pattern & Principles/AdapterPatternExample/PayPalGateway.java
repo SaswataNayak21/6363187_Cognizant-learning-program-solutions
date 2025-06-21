1e82fb23144c68e6ce58afe2d5d63bb2de99dc80
Saswata Kumar Nayak <saswatworkspace711@gmail.com>
2025-06-21 17:26:30 +0530

    Delete Design Pattern & Principles directory

--- Design Pattern & Principles/AdapterPatternExample/PayPalGateway.java
+++
@@ -1,5 +0,0 @@
-public class PayPalGateway {
-    public void sendPayment(double amountInUSD) {
-        System.out.println("Payment of $" + amountInUSD + " processed via PayPal.");
-    }
-}
--- Design Pattern & Principles/AdapterPatternExample/StripeGateway.java
+++
@@ -1,5 +0,0 @@
-public class StripeGateway {
-    public void makePayment(double money) {
-        System.out.println("Payment of $" + money + " processed via Stripe.");
-    }
-}
